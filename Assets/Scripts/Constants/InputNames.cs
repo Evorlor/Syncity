2ec66202169d4d75b08ff33eaff3239fe139f481
Sam Knox <sam@evorlor.com>
2017-07-16 20:15:16 -0400

    added player input

---
+++ Assets/Scripts/Constants/InputNames.cs
@@ -0,0 +1,30 @@
+﻿namespace Syncity
+{
+	public static class InputNames
+	{
+		/// <summary>
+		/// Name of input to render normal vision
+		/// </summary>
+		public const string NormalVision = "Normal Vision";
+
+		/// <summary>
+		/// Name of input to render thermal vision
+		/// </summary>
+		public const string ThermalVision = "Thermal Vision";
+
+		/// <summary>
+		/// Name of input to render night vision
+		/// </summary>
+		public const string NightVision = "Night Vision";
+
+		/// <summary>
+		/// Name of input to render EM Vision
+		/// </summary>
+		public const string EMVision = "EM Vision";
+
+		/// <summary>
+		/// Name of input to render special vision (a combination of normal, night, thermal, and EM visions)
+		/// </summary>
+		public const string SpecialVision = "Special Vision";
+	}
+}
---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace Syncity
+{
+	public class CameraController : MonoBehaviour
+	{
+		private void Update()
+		{
+			if (Input.GetButtonDown(InputNames.NormalVision))
+			{
+				EnableNormalVision();
+			}
+			if (Input.GetButtonDown(InputNames.ThermalVision))
+			{
+				EnableThermalVision();
+			}
+			if (Input.GetButtonDown(InputNames.NightVision))
+			{
+				EnableNightVision();
+			}
+			if (Input.GetButtonDown(InputNames.EMVision))
+			{
+				EnableEMVision();
+			}
+			if (Input.GetButtonDown(InputNames.SpecialVision))
+			{
+				EnableSpecialVision();
+			}
+		}
+
+		/// <summary>
+		/// Makes the camera render normally without any added effects
+		/// </summary>
+		private void EnableNormalVision()
+		{
+			throw new NotImplementedException("Normal Vision");
+		}
+
+		/// <summary>
+		/// Makes the camera render thermal vision
+		/// </summary>
+		private void EnableThermalVision()
+		{
+			throw new NotImplementedException("Thermal Vision");
+		}
+
+		/// <summary>
+		/// Makes the camera render in night vision
+		/// </summary>
+		private void EnableNightVision()
+		{
+			throw new NotImplementedException("Night Vision");
+		}
+
+		/// <summary>
+		/// Makes the camera render EM vision
+		/// </summary>
+		private void EnableEMVision()
+		{
+			throw new NotImplementedException("EM Vision");
+		}
+
+		/// <summary>
+		/// Makes the camera render normal vision, with 3 picture in pictures rendered in night, thermal, and EM visions
+		/// </summary>
+		private void EnableSpecialVision()
+		{
+			throw new NotImplementedException("Special Vision");
+		}
+	}
+}
